ecc14b7ff04d1815283c1a002a447c700347a78c
bhavadharini786-ux <bhavadharini786@gmail.com>
2026-06-09 15:54:18 +0530

    Remove dbtest error

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,14 @@
-
 from flask import Flask, redirect, url_for
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
 from app.utils.db import init_db
-
-# IMPORT BLUEPRINTS
 from app.routes.habit_routes import habit_bp
 from app.routes.auth_routes import auth_bp
 
 load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
@@ -20,26 +18,26 @@ def create_app():
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
-    # INIT DATABASE
+    # Initialize MongoDB
     init_db(app)
 
-    # REGISTER BLUEPRINTS
+    # Register Blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(habit_bp)
 
-   
     @app.route("/")
     def home():
-         return redirect(url_for("auth.login"))
+        return redirect(url_for("auth.login"))
 
+    @app.route("/dbtest")
+    def dbtest():
+        from app.utils.db import mongo
+
+        mongo.db.users.find_one({})
+        return "MongoDB Connected!"
 
     return app
-@app.route("/dbtest")
-def dbtest():
-    from app.utils.db import mongo
 
-    mongo.db.users.find_one({})
-    return "MongoDB Connected!"
 
 
 
